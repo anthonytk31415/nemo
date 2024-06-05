76d9d05ab5e3a12b2e1db582d651949fc4614359
Anthony Trankiem <114188623+anthonytk31415@users.noreply.github.com>
2024-06-05 13:15:57 -0700

    add app for lists with a deliberately failing unit test

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,24 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
 
-assert "Congratulations!" in browser.title
-print("OK")
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_todo_list(self):
+        # check out the home page here: 
+        self.browser.get("http://localhost:8000")
+        
+        # notices the page title and header mention to do lists: 
+        self.assertIn("To-Do", self.browser.title)
+
+        # she is invited to enter to do item straight away: 
+        self.fail("Finish the test!")
+
+
+if __name__ == "__main__":
+    unittest.main()
